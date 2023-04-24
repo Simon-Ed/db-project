46bc8d123ff8d3c6cb66b5688beffa84d3716b6e
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-04-24 13:36:14 +0200

    Fixed bug

--- project/app.py
+++ project/app.py
@@ -1,15 +1,9 @@
 from flask import Flask
 from api import api
 
-#app = Flask(__name__)
-
-def create_app():
-    app = Flask(__name__)
-    api.init_app(app)
-    app.run(debug=True, host='0.0.0.0')
-    return 
+app = Flask(__name__)
 
 # Initialize and run app
-#if __name__ == '__main__':
-#    api.init_app(app)
-#    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    api.init_app(app)
+    app.run(debug=True, host='0.0.0.0')
