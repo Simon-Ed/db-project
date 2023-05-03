ebfedd8bde3c0fd04c11633aa14005592db1ff3e
DanVi007 <danvitrinh@outlook.com>
2023-05-03 20:54:06 +0200

    established connection to db and tested user permissions

--- project/api/api.py
+++ project/api/api.py
@@ -1,16 +1,53 @@
 import http
 from flask import Flask, jsonify
+import pymysql.cursors
+
+# Database configuration
+adminConfig = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'db': 'project_db',
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+publicConfig = {
+    'host': 'localhost',
+    # 'user': 'root',
+    'user': 'general_public',
+    'password': '',
+    'db': 'project_db',
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+publicConnection = pymysql.connect(**publicConfig)
 
 # Method contain API endpoints
+
+
 def init_app(app: Flask):
     @app.route('/courses', methods=['GET'])
     def get_courses():
-        return jsonify({'message':'courses endpoint'}), http.HTTPStatus.OK
+        return jsonify({'message': 'courses endpoint'}), http.HTTPStatus.OK
 
     @app.route('/rooms', methods=['GET'])
     def get_rooms():
-        return jsonify({'message':'rooms endpoint'}), http.HTTPStatus.OK
+        return jsonify({'message': 'rooms endpoint'}), http.HTTPStatus.OK
 
     @app.route('/teachers', methods=['GET'])
     def get_teachers():
-        return jsonify({'message':'teachers endpoint'}), http.HTTPStatus.OK
+        return jsonify({'message': 'teachers endpoint'}), http.HTTPStatus.OK
+
+    @app.route('/test', methods=['GET'])
+    def test_user_permissions():
+        with publicConnection.cursor() as cursor:
+            # query = "SELECT * FROM course;"
+            query = "INSERT INTO institute (institute ,faculty) VALUES ('sa', 'b');"
+            cursor.execute(query)
+            # https://stackoverflow.com/questions/6027271/python-mysql-insert-not-working
+            publicConnection.commit()
+            data = cursor.fetchall()
+            response = jsonify(data)
+        return response
