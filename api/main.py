9495eba7b83019e369b4e15457a0b75ce613f8b3
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-05-08 00:06:48 +0200

    Readded code

--- api/main.py
+++ api/main.py
@@ -25,6 +25,81 @@ def teachers_endpoint():
 
     return jsonify(teachers)
 
+# TODO: Move logic into own modules
+@app.route('/testGeneralPublic', methods=['GET'])
+def test_general_public_permissions_UPDATE():  #
+    with publicConnection.cursor() as cursor:
+        query = "UPDATE course SET name = 'Databaser 3', teacher_id = '1234567890'" \
+                " WHERE course_id = 'IDATG2001' AND semester_id = '2' LIMIT 1"
+        cursor.execute(query)
+        publicConnection.commit()
+        data = cursor.fetchall()
+        response = jsonify(data)
+    return response
+
+@app.route('/testStudent1', methods=['GET'])
+def test_student_permissions_UPDATE():  #
+    with studentConnection.cursor() as cursor:
+        query = "UPDATE course SET name = 'Databaser 3', teacher_id = '1234567890'" \
+                " WHERE course_id = 'IDATG2001' AND semester_id = '2' LIMIT 1"
+        cursor.execute(query)
+        publicConnection.commit()
+        data = cursor.fetchall()
+        response = jsonify(data)
+    return response
+
+@app.route('/testStudent2', methods=['GET'])
+def test_student_update_booking():
+    booking_id = request.args.get('booking_id')
+    start_time = request.args.get('start_time')
+    end_time = request.args.get('end_time')
+    type_update = request.args.get('type')
+    room_id = request.args.get('room_id')
+    booker = request.args.get('booker')
+
+    with studentConnection.cursor() as cursor:
+        query = "UPDATE room_booking \
+                    SET start_time = %s, end_time = %s, type = %s, room_id = %s, booker = %s \
+                    WHERE booking_id = %s"
+        values = (start_time, end_time, type_update, room_id, booker, booking_id)
+        cursor.execute(query, values)
+        studentConnection.commit()
+
+    return "Booking with ID {} was successfully updated".format(booking_id)
+
+@app.route('/testLecturer1', methods=['GET'])
+def test_lecturer_permissions_UPDATE_1():
+    with lecturerConnection.cursor() as cursor:
+        query = "UPDATE course SET name = CASE WHEN name = 'Databaser 1' THEN 'Databaser 3' WHEN name = 'Databaser 3' THEN 'Databaser 1' ELSE name " \
+                "END WHERE course_id = 'IDATG2001' AND semester_id = 2 LIMIT 1;"
+        cursor.execute(query)
+        lecturerConnection.commit()
+        data = cursor.fetchall()
+        response = jsonify(data)
+    return response
+
+@app.route('/testLecturer2', methods=['GET'])
+def test_lecturer_permissions_UPDATE_2():
+    with lecturerConnection.cursor() as cursor:
+        query = "UPDATE teacher SET title = CASE WHEN title = 'Professor' THEN 'rosseforP' ELSE 'Professor' " \
+                "END WHERE personal_id = '0123456789' LIMIT 1;"
+        cursor.execute(query)
+        lecturerConnection.commit()
+        data = cursor.fetchall()
+        response = jsonify(data)
+    return response
+
+
+@app.route('/testAdmin', methods=['GET'])
+def test_admin_permissions_UPDATE():
+    with adminConnection.cursor() as cursor:
+        query = "UPDATE teacher SET title = CASE WHEN title = 'Professor' THEN 'rosseforP' ELSE 'Professor' " \
+                "END WHERE personal_id = '0123456789' LIMIT 1;"
+        cursor.execute(query)
+        adminConnection.commit()
+        data = cursor.fetchall()
+        response = jsonify(data)
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
