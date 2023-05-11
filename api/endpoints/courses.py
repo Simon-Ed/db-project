a2aa65b7b34dafada1f2cf9c6028362609d5447c
SimonEd <simonedn@stud.ntnu.no>
2023-05-11 16:39:54 +0200

    Added endpoint for task 1-4

--- api/endpoints/courses.py
+++ api/endpoints/courses.py
@@ -5,63 +5,55 @@ from db.db_conn import rootConnection
 
 # Show a list of all courses that have not been assigned a lecturer, along with the room number
 # and building name where each course is held.
-def get_courses_roomnr_buildingname_noteacher():
-    with rootConnection.cursor() as cursor:
-        query = "SELECT course.name, Room.room_number, Room.building_name " \
-                "FROM course" \
-                "LEFT JOIN teacher ON course.teacher_id = teacher.personal_id" \
-                "LEFT JOIN lecture ON course.course_id = lecture.course_id" \
-                "LEFT JOIN Room_booking ON lecture.booking_id = Room_booking.booking_id" \
-                "LEFT JOIN Room ON Room_booking.room_id = Room.room_id" \
-                "WHERE teacher_id = NULL;"
-        cursor.execute(query)
-        courses = cursor.fetchall()
-        response = jsonify(courses)
-    return response
+def get_courses_task1():
+    cursor = rootConnection.cursor()
+    query = "SELECT course.name AS course_name, room.room_number, building.building_name FROM course LEFT JOIN teacher ON course.teacher_id = teacher.university_member LEFT JOIN lecture ON course.id = lecture.course_id LEFT JOIN room_booking ON lecture.booking_id = room_booking.id LEFT JOIN room ON room_booking.room_id = room.id LEFT JOIN building ON room.building_id = building.id WHERE course.teacher_id IS NULL;"
+    cursor.execute(query)
+    courses = cursor.fetchall()
+
+    # Map column names to custom field names
+    field_names = [desc[0] for desc in cursor.description]
+    mapped_courses = [dict(zip(field_names, course)) for course in courses]
+
+    return jsonify(mapped_courses)
+
 
 #2. Show a list of all courses taught by a specific teacher in a given semester.
-def get_course_in_given_sem_specific_teacher():
-    with rootConnection.cursor() as cursor:
-        query = "SELECT course.name, university_member.name, semester.name, semester.start_time " \
-                "FROM teacher " \
-                "LEFT JOIN university_member ON teacher.university_member = university_member.id" \
-                "LEFT JOIN course ON teacher.personal_id = course.teacher_id " \
-                "LEFT JOIN semester ON course.semester_id = semester.semester_id " \
-                "WHERE university_member.name = 'Emily' AND semester.name = 'SPRING2023' AND semester.start_time = '2023-01-01 00:00:00'"
-        cursor.execute(query)
-        courses = cursor.fetchall()
-        response = jsonify(courses)
+def get_courses_task2():
+    cursor = rootConnection.cursor()
+    query = ''
+    cursor.execute(query)
+    courses = cursor.fetchall()
+    response = jsonify(courses)
     return response
 
 
 # 3. Show a list of all courses taught in a specific room on a given date and a specific time range.
-def get_course_in_given_room_given_date_specific_time_range():
-    with rootConnection.cursor() as cursor:
-        query = "SELECT lecture.activity, room.room_number, room_booking.start_time, room_booking.end_time " \
-                "FROM lecture " \
-                "LEFT JOIN room_booking ON lecture.booking_id = room_booking.booking_id " \
-                "LEFT JOIN room ON room_booking.room_id = room.room_id " \
-                "WHERE room_booking.start_time " \
-                "BETWEEN '2023-06-08 09:00:00' AND ('2023-06-08 13:15:00') " \
-                "ORDER BY room_booking.start_time ASC, room_booking.end_time ASC"
-        cursor.execute(query)
-        courses = cursor.fetchall()
-        response = jsonify(courses)
-    return response
+def get_courses_task3():
+    cursor = rootConnection.cursor()
+    query = "SELECT lecture.activity, room.room_number, room_booking.start_time, room_booking.end_time FROM lecture LEFT JOIN room_booking ON lecture.booking_id = room_booking.id LEFT JOIN room ON room_booking.room_id = room.id WHERE room_booking.start_time BETWEEN '2023-05-08 09:00:00' AND '2023-06-08 13:15:00' AND room.room_number='R101' ORDER BY room_booking.start_time ASC, room_booking.end_time ASC"
+    cursor.execute(query)
+    courses = cursor.fetchall()
+
+    # Map column names to custom field names
+    field_names = [desc[0] for desc in cursor.description]
+    mapped_courses = [dict(zip(field_names, course)) for course in courses]
+
+    return jsonify(mapped_courses)
 
 
 # 4. Show a list of all courses taught in a specific room on a given date and at a specific time.
-def get_course_in_given_room_given_date_specific_time():
-    with rootConnection.cursor() as cursor:
-        query = "SELECT lecture.activity, room.room_number, room_booking.start_time, room_booking.end_time " \
-                "FROM lecture " \
-                "LEFT JOIN room_booking ON lecture.booking_id = room_booking.booking_id " \
-                "LEFT JOIN room ON room_booking.room_id = room.room_id " \
-                "WHERE room_booking.start_time = 'start_time' AND room_booking.end_time = 'end_time'"
-        cursor.execute(query)
-        courses = cursor.fetchall()
-        response = jsonify(courses)
-    return response
+def get_courses_task4():
+    cursor = rootConnection.cursor()
+    query = "SELECT lecture.activity, room.room_number, room_booking.start_time, room_booking.end_time FROM lecture LEFT JOIN room_booking ON lecture.booking_id = room_booking.id LEFT JOIN room ON room_booking.room_id = room.id WHERE room_booking.start_time = '2023-05-12 09:00:00' AND room.room_number='R101'"
+    cursor.execute(query)
+    courses = cursor.fetchall()
+
+    # Map column names to custom field names
+    field_names = [desc[0] for desc in cursor.description]
+    mapped_courses = [dict(zip(field_names, course)) for course in courses]
+
+    return jsonify(mapped_courses)
 
 
 # 5. Show a list of all courses, along with the name and email of the teacher teaching each course.
