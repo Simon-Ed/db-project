df6ba1a6450d1e8b45e031d57a05389247da1a0e
DanVi007 <danvt@stud.ntnu.no>
2023-05-11 22:35:39 +0200

    added commented out dan config

--- api/db/db_conn.py
+++ api/db/db_conn.py
@@ -9,6 +9,16 @@ rootConfig = {
     'port': "3306"
 }
 
+# Dan sin config
+# rootConfig = {
+#     'host': 'localhost',
+#     'user': 'root',
+#     'password': '',
+#     'database': 'project_db7',
+#     'port': "3307"
+# }
+
 rootConnection = mysql.connector.connect(**rootConfig)
 
 print("DB connected")
+
