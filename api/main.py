3a1d25e614b005f2f3d9868c8ccc808bb0934daf
DESKTOP-148T8UP\Simon <simonedna02@gmail.com>
2023-05-12 03:29:06 +0200

    Added parameters to queries and added default page

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, request
+from datetime import datetime
+import http
+from flask import Flask, jsonify, render_template, request
 from endpoints.courses import *
 from endpoints.rooms import *
 from endpoints.teachers import *
@@ -8,11 +10,31 @@ app = Flask(__name__)
 # Endpoint for testing the 16 queries in the task.
 @app.route('/query-test/<int:endpoint>', methods=['GET'])
 def query_test_endpoint(endpoint):
+    id = request.args.get('id')
+    name = request.args.get('name')
+    surname = request.args.get('surname')
+    semester = request.args.get('semester')
+    starttime = request.args.get('starttime', default='', type=str)
+    endtime = request.args.get('endtime', default='', type=str)
+
+    # Check if the date is formatted correctly
+    if starttime:
+        try:
+            datetime.strptime(starttime, '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            return jsonify({'error': 'Invalid date format'}), http.HTTPStatus.BAD_REQUEST
+        
+    if endtime:
+        try:
+            datetime.strptime(endtime, '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            return jsonify({'error': 'Invalid date format'}), http.HTTPStatus.BAD_REQUEST
+
     tasks = {
         1: get_courses_task1,
-        2: get_courses_task2,
-        3: get_courses_task3,
-        4: get_courses_task4,
+        2: lambda: get_courses_task2(name, semester),
+        3: lambda: get_courses_task3(id, starttime, endtime),
+        4: lambda: get_courses_task4(starttime, id),
         5: get_courses_task5,
         6: get_courses_task6,
         7: get_courses_task7,
@@ -28,121 +50,10 @@ def query_test_endpoint(endpoint):
     }
     return tasks.get(endpoint, lambda: None)()
 
-
-@app.route('/rooms', methods=['GET'])
-def rooms_endpoint():
-    rooms = get_courses_task1()
-
-    return jsonify(rooms)
-
-
-@app.route('/teachers', methods=['GET'])
-def teachers_endpoint():
-    teachers = get_courses_task1()
-
-    return jsonify(teachers)
-#
-# # TODO: Move logic into own modules
-# @app.route('/testGeneralPublic', methods=['GET'])
-# def test_general_public_permissions_UPDATE():  #
-#     with publicConnection.cursor() as cursor:
-#         query = "UPDATE course SET name = 'Databaser 3', teacher_id = '1234567890'" \
-#                 " WHERE course_id = 'IDATG2001' AND semester_id = '2' LIMIT 1"
-#         cursor.execute(query)
-#         publicConnection.commit()
-#         data = cursor.fetchall()
-#         response = jsonify(data)
-#     return response
-#
-# @app.route('/testStudent1', methods=['GET'])
-# def test_student_permissions_UPDATE():  #
-#     with studentConnection.cursor() as cursor:
-#         query = "UPDATE course SET name = 'Databaser 3', teacher_id = '1234567890'" \
-#                 " WHERE course_id = 'IDATG2001' AND semester_id = '2' LIMIT 1"
-#         cursor.execute(query)
-#         publicConnection.commit()
-#         data = cursor.fetchall()
-#         response = jsonify(data)
-#     return response
-#
-# @app.route('/testStudent2', methods=['GET'])
-# def test_student_update_booking():
-#     booking_id = request.args.get('booking_id')
-#     start_time = request.args.get('start_time')
-#     end_time = request.args.get('end_time')
-#     type_update = request.args.get('type')
-#     room_id = request.args.get('room_id')
-#     booker = request.args.get('booker')
-#
-#     with studentConnection.cursor() as cursor:
-#         query = "UPDATE room_booking \
-#                     SET start_time = %s, end_time = %s, type = %s, room_id = %s, booker = %s \
-#                     WHERE booking_id = %s"
-#         values = (start_time, end_time, type_update, room_id, booker, booking_id)
-#         cursor.execute(query, values)
-#         studentConnection.commit()
-#
-#     return "Booking with ID {} was successfully updated".format(booking_id)
-#
-# @app.route('/testLecturer1', methods=['GET'])
-# def test_lecturer_permissions_UPDATE_1():
-#     with lecturerConnection.cursor() as cursor:
-#         query = "UPDATE course SET name = CASE WHEN name = 'Databaser 1' THEN 'Databaser 3' WHEN name = 'Databaser 3' THEN 'Databaser 1' ELSE name " \
-#                 "END WHERE course_id = 'IDATG2001' AND semester_id = 2 LIMIT 1;"
-#         cursor.execute(query)
-#         lecturerConnection.commit()
-#         data = cursor.fetchall()
-#         response = jsonify(data)
-#     return response
-#
-# @app.route('/testLecturer2', methods=['GET'])
-# def test_lecturer_permissions_UPDATE_2():
-#     with lecturerConnection.cursor() as cursor:
-#         query = "UPDATE teacher SET title = CASE WHEN title = 'Professor' THEN 'rosseforP' ELSE 'Professor' " \
-#                 "END WHERE personal_id = '0123456789' LIMIT 1;"
-#         cursor.execute(query)
-#         lecturerConnection.commit()
-#         data = cursor.fetchall()
-#         response = jsonify(data)
-#     return response
-#
-#
-# @app.route('/testAdmin', methods=['GET'])
-# def test_admin_permissions_UPDATE():
-#     with adminConnection.cursor() as cursor:
-#         query = "UPDATE teacher SET title = CASE WHEN title = 'Professor' THEN 'rosseforP' ELSE 'Professor' " \
-#                 "END WHERE personal_id = '0123456789' LIMIT 1;"
-#         cursor.execute(query)
-#         adminConnection.commit()
-#         data = cursor.fetchall()
-#         response = jsonify(data)
-#     return response
-#
-
-# add course to time schedule 
-@app.route('/add-to-schedule')
-def add_course_to_time_schedule():
-    user = request.args.get('user', default='', type=int)
-    course = request.args.get('course', default='', type=int)
-    with rootConnection.cursor() as cursor:
-        query = "INSERT INTO user_schedule( user_schedule.user_id, user_schedule.course_id) VALUES(%s, %s);"
-        cursor.execute(query, (user,course,))
-        rootConnection.commit()
-        courses = cursor.fetchall()
-        response = jsonify(courses)
-    return response
-
-# view personal time schedule 
-@app.route('/view-schedule')
-def view_time_schedule():
-    user = request.args.get('user', default='', type=int)
-    with rootConnection.cursor() as cursor:
-        query = "SELECT * FROM user_time_schedule WHERE user_time_schedule.user_id=%s;" 
-        cursor.execute(query, (user,))
-        courses = cursor.fetchall()
-        response = jsonify(courses)
-    return response
-
+@app.route('/')
+def default_path():
+    URL = "http://localhost:5000"
+    return render_template('index.html', URL=URL)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
